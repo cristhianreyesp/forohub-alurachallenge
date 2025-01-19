5895616cb6e525b4b6e378955fdbbf4fd3f6a12e
cristhianreyesp <cristhianreyesp04@gmail.com>
2025-01-19 04:03:00 -0500

    topico controller actualziado el crud

--- src/main/java/cristhianrp/forohub/api/controller/TopicoController.java
+++ src/main/java/cristhianrp/forohub/api/controller/TopicoController.java
@@ -1,6 +1,7 @@
 package cristhianrp.forohub.api.controller;
 
 import cristhianrp.forohub.api.domain.topicos.*;
+import cristhianrp.forohub.api.domain.topicos.dto.TopicoActualizar;
 import cristhianrp.forohub.api.domain.topicos.dto.TopicoCreacion;
 import cristhianrp.forohub.api.domain.topicos.dto.TopicoMostar;
 import cristhianrp.forohub.api.infra.errores.ValidacionException;
@@ -38,9 +39,49 @@ public class TopicoController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
+
     // Nuevo método para listar todos los tópicos
     @GetMapping
     public ResponseEntity<Page<TopicoMostar>> listarTopicos(@PageableDefault(size = 5) Pageable paginacion) {
         return ResponseEntity.ok(topicoRepository.findAll(paginacion).map(TopicoMostar::new));
     }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity actualizar(@RequestBody @Valid TopicoActualizar datos , @PathVariable Long id){
+
+        try{
+            var detalleTopico = topicoService.actualizar(id, datos);
+            return ResponseEntity.ok(detalleTopico);
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity mostarTopicoPorId(@PathVariable Long id){
+
+        var topicoOptional = topicoRepository.findById(id);
+
+        if(topicoOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No hay topico con es ID:" + id);
+        }
+        var topico = topicoOptional.get();
+        return ResponseEntity.ok(new TopicoMostar(topico));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity eliminar(@PathVariable Long id){
+
+        var topico = topicoRepository.findById(id);
+        if(topico.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró ningún tópico con id: " + id);
+        }
+
+        topicoRepository.delete(topico.get());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Tópico eliminado correctamente");
+    }
+
+
+
 }
